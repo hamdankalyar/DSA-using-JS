77091520bf516d8514d4cfb59b336d22f7dfbc7c
Hamdan <hamdan.kalyar@outlook.com>
2024-08-11 16:41:16 +0500

    updated the second min and second max and also update the uniqueFrequencies code

--- array/FindMinMax.js
+++ array/FindMinMax.js
@@ -1,29 +1,42 @@
 
 // -------------------------- find min and max in array --------------------------
 
-let array = [-1, 2, 3, 4, 5, 6, 7, 8, 9, 100]
+// let array = [-1, 0, 2, 3, 4, 5, 6, 7, 8, 9, 100]
+let array = [3, 4, 1]
+// 3,4,1
 function findMin(array) {
     let min = Infinity
+    let secondMin = Infinity
     array.forEach(element => {
         if (min > element) {
+            secondMin = min
             min = element
+
+        }
+        else if (secondMin > element && element !== min) {
+            secondMin = element
         }
     })
-    return min
+    return [min, secondMin]
 }
 
-console.log("Min number in the array", findMin(array));
-
+console.log("Min number and second min in the array", findMin(array));
+// 2,3,4 
 function findMax(array) {
     let max = -Infinity
+    let secondMax = -Infinity
     array.forEach(element => {
         if (max < element) {
+            secondMax = max
             max = element
         }
+        else if (secondMax < element && element !== max) {
+            secondMax = element
+        }
     })
-    return max
+    return [max, secondMax]
 }
-console.log("Max number in the array", findMax(array));
+console.log("Max number and second max in the array", findMax(array));
 
 
 // -------------------------- find the 8 is in the power of 2 ----------------
@@ -32,25 +45,25 @@ console.log("Max number in the array", findMax(array));
 
 
 // -------------------------- find unique in the array  -----------------------------
-let array = [1, 2, 3, 1, 2]
-function findUniqueElementsInArray(array) {
-    let uniqueElements = [];
+// let array = [1, 2, 3, 1, 2]
+// function findUniqueElementsInArray(array) {
+//     let uniqueElements = [];
 
-    array.forEach(element => {
+//     array.forEach(element => {
 
-        let found = false;
-        for (let index = 0; index < array.length; index++) {
-            if (element === array[index]) {
-                found = true;
-                break; // Exit the loop early since we found a match
-            }
-        }
-        if (!found) {
-            uniqueElements.push(element);
-        }
-    });
-    return uniqueElements
-}
+//         let found = false;
+//         for (let index = 0; index < array.length; index++) {
+//             if (element === array[index]) {
+//                 found = true;
+//                 break; // Exit the loop early since we found a match
+//             }
+//         }
+//         if (!found) {
+//             uniqueElements.push(element);
+//         }
+//     });
+//     return uniqueElements
+// }
 
-console.log("Unique elements in the array", findUniqueElementsInArray(array))
+// console.log("Unique elements in the array", findUniqueElementsInArray(array))
 
--- array/HaveUniqueFrequencies.js
+++ array/HaveUniqueFrequencies.js
@@ -14,30 +14,27 @@ function ElementsFrequencies(array) {
     return arrayFrequency;
 }
 console.log(ElementsFrequencies([1, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5]))
-let array = ElementsFrequencies([1, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5]);
+let array = ElementsFrequencies([1, 2, 2, 2, 2, 2, 5, 5, 5, 5]);
 
 
 
 function HaveUniqueFrequencies(arrayFrequency) {
 
     let isUnique = true
-    let frequenciesOfNo = Object.entries(arrayFrequency)
+
     for (let key in arrayFrequency) {
 
-        for (let i = 0; i < frequenciesOfNo.length; i++) {
-            // console.log(typeof Number(key))
-            if (key != frequenciesOfNo[i][0]) {
-                // console.log("here it the key",Number(key),"this is the frequenciesOfNo[i][0]", frequenciesOfNo[i][0])
-                // console.log("Statement Number(key)!==frequenciesOfNo[i][0]",Number(key)!==frequenciesOfNo[i][0])
-                if (arrayFrequency[key] == frequenciesOfNo[i][1]) {
+        for (let key2 in arrayFrequency) {
+            if (key != key2) {
+                if (arrayFrequency[key] == arrayFrequency[key2]) {
                     isUnique = false
                     break
                 }
             }
-
         }
+
     }
     return isUnique
 }
 
-console.log(HaveUniqueFrequencies(array))
+console.log(HaveUniqueFrequencies(array));
